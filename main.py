fd86696ff90d68863014a63b00fa3a72286d1df2
myomyintaung <myomyintaung064@gmail.com>
2021-10-06 11:30:58 +0630

    Finished basic Script
    Can upgrade with GUI

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
+from datetime import datetime
+from requests.api import head
 from requests.models import Response
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 USERNAME = "myomyintaung"
 TOKEN = "adfadsfesdfasdfasdfa"
+GRAPH_ID = "graph1"
 
+# Creating pixela account with POST method
 user_paramas = {
     "token": "adfadsfesdfasdfasdfa",
     "username": "myomyintaung",
@@ -20,16 +24,51 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
-
-graph_config = {
-    "id": "graph1",
+# Creating Graph with POST Method
+graph_creating_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+graph_creating_config = {
+    "id": GRAPH_ID,
     "name": "Coding Habit",
     "unit": "hours",
     "type": "float",
     "color": "momiji",
 }
 
-response = requests.post(
-    url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(
+#     url=graph_creating_endpoint, json=graph_creating_config, headers=headers)
+# print(response.text)
+
+pixel_creating_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime(2020, 10, 5)
+
+# We can change to any format of Date with strftime() method
+
+# Creating pixel With POST Method
+pixel_creating_config = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "1"
+}
+
+# response = requests.post(url=pixel_creating_endpoint,
+#                          json=pixel_creating_config, headers=headers)
+# print(response.text)
+
+
+# Updating the exisiting pixel with PUT Method
+pixel_updating_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+pixel_updating_config = {
+    "quantity": "1",
+
+}
+
+# response = requests.put(url=pixel_updating_endpoint,
+#                         json=pixel_updating_config, headers=headers)
+# print(response.text)
+
+# Deleting the exisiting pixel wiht DELETE method
+piexl_deleting_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=piexl_deleting_endpoint, headers=headers)
 print(response.text)
